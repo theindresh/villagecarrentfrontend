3b2a1262ce34e8cb2e2753ea26fe9ec7fe186ea6
theindresh <indreshbharati77@gmail.com>
2024-06-02 15:18:05 +0530

    done

--- src/Authentication/SignUp.jsx
+++ src/Authentication/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useTheme } from '../Components/ThemeContext/ThemeContext';
-import googleImg from '../assets/web_light_sq_SU.svg'; // Ensure the path to the image is correct
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   const { isDarkMode } = useTheme();
@@ -51,10 +51,10 @@ const SignUp = () => {
   };
 
   return (
-    <div className={`${isDarkMode ? 'bg-black text-white' : 'bg-white text-black'} min-h-screen flex items-center justify-center pt-16 px-4 sm:px-6 lg:px-8`}>
-      <div className="max-w-md w-full shadow-2xl px-16 pb-4 mt-4 rounded-3xl space-y-8 bg-white dark:bg-gray-900">
+    <div className={`${isDarkMode ? 'bg-gray-800 text-white' : 'bg-white text-black'} min-h-screen flex items-center justify-center pt-16 px-4 sm:px-6 lg:px-8`}>
+      <div className="max-w-md w-full shadow-2xl px-16 pb-4 mt-4 rounded-3xl space-y-8">
         <div className="text-center">
-          <h2 className="mt-6 text-3xl font-extrabold dark:text-white">Create an account</h2>
+          <h2 className="mt-6 text-3xl font-extrabold">{isDarkMode ? 'Create an account (Dark Mode)' : 'Create an account'}</h2>
         </div>
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
           {error && <div className="text-red-500">{error}</div>}
@@ -68,7 +68,7 @@ const SignUp = () => {
                 type="text"
                 autoComplete="name"
                 required
-                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 dark:border-gray-700 placeholder-gray-500 dark:placeholder-gray-400 rounded-3xl focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className={`appearance-none relative block w-full px-3 py-2 border placeholder-gray-500 rounded-3xl focus:outline-none focus:ring focus:border-blue-300 sm:text-sm ${isDarkMode ? 'border-gray-700 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-700' : 'border-gray-300'}`}
                 placeholder="Name"
                 value={name}
                 onChange={handleInputChange(setName)}
@@ -82,7 +82,7 @@ const SignUp = () => {
                 type="email"
                 autoComplete="email"
                 required
-                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 dark:border-gray-700 placeholder-gray-500 dark:placeholder-gray-400 rounded-3xl focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className={`appearance-none relative block w-full px-3 py-2 border placeholder-gray-500 rounded-3xl focus:outline-none focus:ring focus:border-blue-300 sm:text-sm ${isDarkMode ? 'border-gray-700 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-700' : 'border-gray-300'}`}
                 placeholder="Email address"
                 value={email}
                 onChange={handleInputChange(setEmail)}
@@ -96,7 +96,7 @@ const SignUp = () => {
                 type={showPassword ? "text" : "password"}
                 autoComplete="current-password"
                 required
-                className="appearance-none block w-full px-3 py-2 border border-gray-300 dark:border-gray-700 placeholder-gray-500 dark:placeholder-gray-400 rounded-3xl focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                className={`appearance-none relative block w-full px-3 py-2 border placeholder-gray-500 rounded-3xl focus:outline-none focus:ring focus:border-blue-300 sm:text-sm ${isDarkMode ? 'border-gray-700 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-700' : 'border-gray-300'}`}
                 placeholder="Password"
                 value={password}
                 onChange={handleInputChange(setPassword)}
@@ -113,39 +113,40 @@ const SignUp = () => {
               </div>
             </div>
           </div>
-          <div className="text-center text-gray-500 dark:text-gray-400 text-sm">
+          <div className="text-center text-gray-500 text-sm">
             By continuing, you agree to our <a href="/" className="text-blue-600">terms of service</a>.
           </div>
           <div>
             <button
               type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white ${isDarkMode ? 'bg-gray-700 hover:bg-gray-600 focus:ring-blue-200' : 'bg-blue-500 hover:bg-blue-600 focus:ring-indigo-500'}`}
             >
               Sign up
             </button>
           </div>
           <div className="flex items-center justify-center mt-4">
-            <span className="text-gray-500 dark:text-gray-400">or</span>
+            <span className="text-gray-500 text-sm">or</span>
           </div>
           <div>
             <button
               type="button"
-              className="w-full flex justify-center py-2 px-4 border border-gray-300 dark:border-gray-700 rounded-md shadow-sm text-sm font-medium text-gray-900 dark:text-white bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className={`w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium ${isDarkMode ? 'text-gray-300 bg-gray-700 hover:bg-gray-600 focus:ring-blue-200' : 'text-gray-900 bg-white hover:bg-gray-50 focus:ring-indigo-500'}`}
             >
-              <img className="h-5 w-5 mr-2" src={googleImg} alt="Google logo" />
+              
               Continue with Google
             </button>
           </div>
           <div className="flex items-center justify-center mt-6">
-            <span className="text-sm text-gray-600 dark:text-gray-400">Already have an account? </span>
-            <a href="/signin" className="text-sm font-medium text-indigo-600 hover:text-indigo-500 ml-1">
-              Sign in here
-            </a>
+            <span className="text-sm text-gray-600">Already have an account? </span>
+            <Link to='/login' className="text-sm font-medium text-indigo-600 hover:text-indigo-500 ml-1">
+              Sign in
+            </Link>
           </div>
         </form>
       </div>
     </div>
   );
-};
+}
 
 export default SignUp;
+             
